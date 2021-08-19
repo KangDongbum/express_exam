27327b69eb70e8a7a663dcf9fd290b91d1467468
KangDongbum <eaststar0201@naver.com>
2021-08-19 18:15:52 +0900

    nodejs 게시판

---
+++ express_exam11/app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const logger = require('./lib/logger');
+
+/** 라우터 **/
+const indexRouter = require('./routes');
+const boardRouter = require('./routes/board');
+
+const app = express();
+
+dotenv.config(); // .env -> process.env 하위 속성으로 추가
+
+app.set('PORT', process.env.PORT || 3000);
+app.set("view engine", "html");
+nunjucks.configure(path.join(__dirname, 'views'),{
+	express : app,
+	watch : true,
+});
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+app.use("/", express.static(path.join(__dirname, 'public')));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+		resave : false,
+		saveUninitialized : true,
+		secret : process.env.COOKIE_SECRET,
+		name : "dbssid",
+}));
+
+/** 라우터 등록 **/
+app.use('/', indexRouter);
+app.use('/board', boardRouter);
+
+
+app.use((req,res,next)=>{ // 없는페이지 처리 라우터
+	// next(에러객체), throw 에러객체 // 404 Status Code - NOT FOUND
+	const error = new Error(`${req.url}는 없는 페이지 입니다.`);
+	error.status = 404;
+	next(error);
+});
+
+app.use((err,req,res,next) => { // 오류처리 미들웨어(라우터)
+	//next(에러객체), throw 에러객체
+	const data = {
+		message : err.message,
+		status : err.status || 500,
+		stack : err.stack,
+	};
+	
+	/** 로그 기록 S**/
+	logger(`[${data.status}]${data.message}`, 'error');
+	logger(data.stack, 'error');
+	/** 로그 기록 E**/
+	
+	if (process.env.NODE_ENV === 'production'){
+		delete data.stack;
+	}
+
+	return res.status(data.status).render("error", data);
+});
+
+app.listen(app.get('PORT'), () => {
+	console.log(app.get('PORT'), "번 포트에서 서버 대기중...");
+});
