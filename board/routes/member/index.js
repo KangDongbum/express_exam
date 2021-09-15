4dbd7272c01c560c1bdf93be5d3ff7712ea79e75
KangDongbum <eaststar0201@naver.com>
2021-09-15 20:37:14 +0900

    nodejs 게시판

--- board/routes/member/index.js
+++ board/routes/member/index.js
@@ -1,3 +1,5 @@
+const member = require("../../models/member"); // 회원 model
+const { joinValidator } = require("../../middlewares/member"); //유효성검사 미들웨어
 const express = require('express');
 const router = express.Router();
 
@@ -11,8 +13,15 @@ router.route("/join")
 	})
 	
 	/**  회원 가입 처리 */
-	.post((req, res) =>{
+	.post(joinValidator, async (req, res) =>{
+		const memNo = await member.join(req.body);
+		if(memNo){ // 회원 가입 성공
+			// 로그인 처리 -> 메인페이지 이동
+		}
 		
+		// 실패 -> 메세지 출력
+		
+		res.send("");
 	});
 	
 router.route("/login")
